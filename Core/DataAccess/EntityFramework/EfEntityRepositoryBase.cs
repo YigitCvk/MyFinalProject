edd351c85b9edf3026413920500d201fb0baa816
Yiğit Çevik <yigitcevik_01@hotmail.com>
2021-02-06 22:55:26 +0300

    06.02.2021

---
+++ Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,63 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Core.DataAccess.EntityFramework
+{
+    public class EfEntityRepositoryBase<TEntity,TContex>:IEntityRepository<TEntity>
+    
+        where TEntity: class , IEntity , new()
+        where TContex:DbContext,new()
+    {
+        public void Add(TEntity entity)
+        {
+            using (TContex contex = new TContex())//yazılan nesnler iş bitince bellekten atar//using=Idiposable pattern implementation of c#
+            {
+                var adedEntity = contex.Entry(entity);
+                adedEntity.State = EntityState.Added;
+                contex.SaveChanges();
+            }
+        }
+
+        public void Delete(TEntity entity)
+        {
+            using (TContex contex = new TContex())//yazılan nesnler iş bitince bellekten atar//using=Idiposable pattern implementation of c#
+            {
+                var deletedEntity = contex.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                contex.SaveChanges();
+            }
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            using (TContex context = new TContex())
+            {
+                return context.Set<TEntity>().SingleOrDefault(filter);
+
+            }
+        }
+
+        public List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null)
+        {
+            using (TContex context = new TContex())
+            {
+                return filter == null ? context.Set<TEntity>().ToList() : context.Set<TEntity>().Where(filter).ToList();
+            }
+        }
+
+        public void Update(TEntity entity)
+        {
+            using (TContex contex = new TContex())//yazılan nesnler iş bitince bellekten atar//using=Idiposable pattern implementation of c#
+            {
+                var updatedEntity = contex.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                contex.SaveChanges();
+            }
+        }
+    }
+}
--- DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -1,4 +1,5 @@
-﻿using DataAccess.Abstract;
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
 using Entities.Concrete;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -10,53 +11,8 @@ using System.Text;
 namespace DataAccess.Concrete.EntityFramework
 {
     //NuGet
-    public class EfProductDal : IProductDal
+    public class EfProductDal : EfEntityRepositoryBase<Product,NorthwindContext>,IProductDal
     {
-        public void Add(Product entity)
-        {
-            using (NorthwindContext contex=new NorthwindContext())//yazılan nesnler iş bitince bellekten atar//using=Idiposable pattern implementation of c#
-            {
-                var adedEntity = contex.Entry(entity);
-                adedEntity.State = EntityState.Added;
-                contex.SaveChanges();
-            }
-        }
-
-        public void Delete(Product entity)
-        {
-            using (NorthwindContext contex = new NorthwindContext())//yazılan nesnler iş bitince bellekten atar//using=Idiposable pattern implementation of c#
-            {
-                var deletedEntity = contex.Entry(entity);
-                deletedEntity.State = EntityState.Deleted;
-                contex.SaveChanges();
-            }
-        }
-
-        public Product Get(Expression<Func<Product, bool>> filter)
-        {
-            using (NorthwindContext context=new NorthwindContext())
-            {
-                return context.Set<Product>().SingleOrDefault(filter);
-
-            }   
-        }
-
-        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
-        {
-            using (NorthwindContext context=new NorthwindContext())
-            {
-                return filter == null ? context.Set<Product>().ToList() : context.Set<Product>().Where(filter).ToList();
-            }
-        }
-
-        public void Update(Product entity)
-        {
-            using (NorthwindContext contex = new NorthwindContext())//yazılan nesnler iş bitince bellekten atar//using=Idiposable pattern implementation of c#
-            {
-                var updatedEntity = contex.Entry(entity);
-                updatedEntity.State = EntityState.Modified;
-                contex.SaveChanges();
-            }
-        }
+        
     }
 }
