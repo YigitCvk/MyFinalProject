f799db0e060bba7fc762ea5b22b3679627dc5e3b
Yiğit Çevik <yigitcevik_01@hotmail.com>
2021-02-11 00:23:53 +0300

    10.02.2021

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -9,7 +9,7 @@ namespace Business.Constants
     {
         public static string ProductAdded = "Ürün eklendi";
         public static string ProductNameInvalid = "Ürün ismi geçersiz";
-        internal static string MaintenanceTime="Sistem BAkımda";
-        internal static string ProductsListed="Ürünler Listelendi";
+        public static string MaintenanceTime="Sistem BAkımda";
+        public static string ProductsListed="Ürünler Listelendi";
     }
 }
