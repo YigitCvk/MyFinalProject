a251599f62b11c267056888d1aedcebcce02c583
Yiğit Çevik <yigitcevik_01@hotmail.com>
2021-02-10 22:29:50 +0300

    07.02.2021

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Abstract;
+using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
@@ -16,6 +17,19 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
+        public IResult Add(Product product)
+        {
+            //businnes codes
+
+            if (product.ProductName.Length<2)
+            {
+                return new ErrorResult("Ürün ismi min 2 Karakter olmalıdır");
+            }
+            _productDal.Add(product);
+            
+            return new SuccessResult();
+        }
+
         public List<Product> GetAll()
         {
             //İş Kodları
@@ -28,6 +42,11 @@ namespace Business.Concrete
         public List<Product> GetAllByCatergoryId(int id)
         {
             return _productDal.GetAll(p => p.CategoryId == id);
+        }
+
+        public Product GetById(int productId)
+        {
+            return _productDal.Get(p=>p.ProductId==productId);
         }
 
         public List<Product> GetByUnitPrice(decimal min, decimal max)
---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+      
+        public Result(bool success, string message):this(success)//resultu işaret eder ve overloading yaptığımız kodla beraber çalışır
+        {
+            Message = message;
+            
+        }
+        public Result(bool success)//overloading
+        {
+            Success = success;
+        }
+
+        public bool Success { get; }
+
+        public string Message { get; }
+
+    }
+}
